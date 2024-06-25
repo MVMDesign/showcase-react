7fbc2d5de3551ed9829d33f7132d8965912b00cb
MVM Design <marijkevanmol15@gmail.com>
2024-06-25 13:54:51 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Footer.jsx";
+import Header from "./Header.jsx";
+
+function App() {
+
+
+
+  return(
+    <>
+      <Header /> 
+      
+      <Footer />  
+    </>
+  );
+  }
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+
+    return(
+
+        <footer>
+            <p>Copyright &copy;MVMDesign  {new Date().getFullYear()} </p>
+        </footer>
+    );
+}
+
+export default Footer;
