60041b2e3990391c3673de8777dcfea1c2dee64b
MVM Design <marijkevanmol15@gmail.com>
2024-06-25 15:17:57 +0200

    #	modified:   package-lock.json #	modified:   package.json #	modified:   src/Footer.jsx #	modified:   src/Header.jsx #	modified:   src/index.css #	new file:   tailwind.config.js

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,14 +4,18 @@ function Header() {
             <h1>My App</h1>
             <nav>
                 <ul>
-                    <li><a href="#">Home</a></li>
+                    <li><a class="text-3xl font-bold underline" href="#">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Services</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
             </nav>
             <hr />
+            <h2 class="text-3xl font-bold underline">
+          Hello world!
+        </h2>
         </header>
+
     );
 }
 
