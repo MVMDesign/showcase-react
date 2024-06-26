36209285dd33d7d2f726964a02c03231da2f915a
MVM Design <marijkevanmol15@gmail.com>
2024-06-26 13:01:59 +0200

    tailwind css

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,18 @@
 function Header() {
     return (
+        <>
+       
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="stylesheet" href="./index.css" />
+            <title>Document</title>
+        </head>
+        <body>
+            
+        </body>
+        </html>
         <header>
             <h1>My App</h1>
             <nav>
@@ -15,7 +28,7 @@ function Header() {
           Hello world!
         </h2>
         </header>
-
+        </>
     );
 }
 
